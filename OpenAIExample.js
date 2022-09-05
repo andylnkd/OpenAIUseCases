e50e4e67e3f8475b785ddca1ff3e4fdc38c70b8b
andylnkd <dhalwani1@gmail.com>
2022-09-05 20:27:49 +0000

    got it working

--- OpenAIExample.js
+++ OpenAIExample.js
@@ -1,26 +1,34 @@
 const { Configuration, OpenAIApi } = require("openai");
 // import * as dotenv from 'dotenv'
 // dotenv.config();
-require('dotenv').config()
+require("dotenv").config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-console.log(openai)
+// console.log(openai);
 
 async function myfunction() {
-    var response;
-     response = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: "Write a creative ad for the following product to run on Facebook aimed at parents:\n\nProduct: Learning Room is a virtual environment to help students from kindergarten to high school excel in school.",
-        temperature: 0.5,
-        max_tokens: 60,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-      });
+  
+  const response = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt:
+      "Write a creative ad for the following product to run on Facebook aimed at parents:\n\nProduct: Learning Room is a virtual environment to help students from kindergarten to high school excel in school.",
+    temperature: 0.5,
+    max_tokens: 60,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+  
+  try {
+      document = await response.data;
+    } catch (err) {
+        console.log("Error");
+    }
     
-  }
+    console.log(JSON.stringify(document.choices));
+}
+myfunction();
